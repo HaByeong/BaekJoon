3ba0319a33d3c8413b977247fd56476443d8e167
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-01-06 12:19:11 +0900

    27866번

---
+++ 문자와 문자열/27866번.py
@@ -0,0 +1,14 @@
+# arr = input().split() 이건 에러가 난다!!!!!!
+#arr = list(input())
+#n = int(input())
+#print(arr[n - 1])
+
+string = input()
+n = int(input())
+
+wordList = []
+for word in string:
+    wordList.append(word)
+print(wordList[n - 1])
+
+

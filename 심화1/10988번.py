a27ee795b01644e478fd5f369960fee89d1fa27b
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-01-07 10:46:57 +0900

    정석 풀이 추가

--- 심화1/10988번.py
+++ 심화1/10988번.py
@@ -1,8 +1,17 @@
+# 내가 푼 것
 arr = input()
-
 arr2 = arr[::-1]
 
 if arr == arr2:
+    print(1)
+else:
+    print(0)
+
+
+#정석 풀이
+arr = input()
+
+if arr == arr[::-1]:
     print(1)
 else:
     print(0)

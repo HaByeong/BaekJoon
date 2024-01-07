527ca4e4f392d99305c4d0b0f8b292e1f1150600
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-01-07 20:33:41 +0900

    1316번

---
+++ 심화1/1316번.py
@@ -0,0 +1,32 @@
+import sys
+# 나의 풀이 -> 맞춤
+#문자열 끼리 비교시 슬라이싱!
+n = int(input())
+cnt = 0
+
+for _ in range(n):
+    string = sys.stdin.readline()
+    numCheck = 0
+    for i in range(len(string) - 1):
+        if string[i] != string[i + 1]:
+            for j in range(i + 2, len(string)):
+                if string[i] == string[j]:
+                    numCheck = -1
+    if numCheck == 0:
+        cnt += 1
+print(cnt)
+
+# 정석 풀이 -> find()를 이용하라!
+n = int(input())
+cnt = n
+
+for _ in range(n):
+    word = input()
+    for i in range(len(word) - 1):
+        if word.find(word[i]) > word.find(word[i + 1]):
+            cnt -= 1
+            break
+print(cnt)
+
+
+
